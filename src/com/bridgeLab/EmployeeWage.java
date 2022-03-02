56b28b068d6468a1417bc231ee22778b6539c994
JAISHRI <sjayashree529@gmail.com>
2022-03-02 16:10:48 +0530

    First commit

--- src/com/bridgeLab/EmployeeWage.java
+++ src/com/bridgeLab/EmployeeWage.java
@@ -4,12 +4,9 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome To main");
         int randVal = (int) (Math.floor(Math.random()*10)%2);
-          int wagePerHr=20;
         if(randVal==1) {
-            int empHr = 8;
-            int empWage = wagePerHr * empHr;
 
-            System.out.print(empWage);
+            System.out.print("Employee is Present");
         }
         else
             System.out.println("Employee is Absent");
