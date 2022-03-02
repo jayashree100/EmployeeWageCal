83d4e98739d37492c7b41d9d0ff5319ffb8510c0
JAISHRI <sjayashree529@gmail.com>
2022-03-02 15:05:55 +0530

    UC1

--- src/com/bridgeLab/EmployeeWage.java
+++ src/com/bridgeLab/EmployeeWage.java
@@ -3,6 +3,12 @@ package com.bridgeLab;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome To main");
+        int randVal = (int) (Math.floor(Math.random()*10)%2);
+
+        if(randVal==1)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is Absent");
 
     }
 }
